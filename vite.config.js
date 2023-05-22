2cd71d78a10081feb53f88952f9df331e0ecff6d
Tzyber <134324777+Tzyber@users.noreply.github.com>
2023-05-22 21:27:07 +0200

    impl

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,14 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  module: {
+    publicPath: process.env.NODE_ENV === "production" ? "/aboutme/" : "/",
+  },
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+
   }
 })
