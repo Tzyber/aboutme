9ab1df5cad8a8a7fbe509a17903165590ea19541
Tzyber <134324777+Tzyber@users.noreply.github.com>
2023-05-26 16:42:53 +0200

    typewriting effect

--- src/components/About-me.vue
+++ src/components/About-me.vue
@@ -10,15 +10,32 @@ defineProps({
 <template>
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
-    <h3>
+    <h3 class="typed-out" >
       Web Designer and Web Developer
-
-
     </h3>
   </div>
 </template>
 
 <style scoped>
+.typed-out{
+  overflow: hidden;
+  border-right: .15em solid orange;
+  white-space: nowrap;
+  animation:
+      typing 3s forwards,
+  blink-caret .75s step-end infinite;
+  font-size: 1.2rem;
+  width: 0;
+}
+@keyframes typing {
+  from { width: 0 }
+  to { width: 100% }
+}
+@keyframes blink-caret {
+  from, to { border-color: transparent }
+  50% { border-color: orange; }
+}
+
 h1 {
   font-weight: 500;
   font-size: 2.6rem;
